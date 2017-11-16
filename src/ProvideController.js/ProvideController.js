e499e7bd89fb0475e148d6ec4a09245c26ae993a
Nir Yosef <niryo@wix.com>
2017-11-16 19:28:42 +0200

    configure first test

---
+++ src/ProvideController.js/ProvideController.js
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import PropTypes from 'prop-types'; 
+
+export class ProvideController extends React.Component {
+  getChildContext() {
+    const controllers = this.context.controllers || {};
+    controllers[this.props.controller.name] = this.props.controller;
+    return {controllers};
+  }
+
+    render() {
+      return this.props.children;
+    }
+}
+
+ProvideController.childContextTypes = {
+  controllers: PropTypes.object
+};
+
+ProvideController.contextTypes = {
+  controllers: PropTypes.object
+};
---
+++ src/ProvideController.js/ProvideController.spec.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { mount } from 'enzyme';
+import { ProvideController } from './ProvideController';
+import PropTypes from 'prop-types'; 
+
+class ChildComponent extends React.Component {
+  render() {
+    return <div data-hook="controllerName">{this.context.controllers.testController.name}</div>
+  }
+}
+
+ChildComponent.contextTypes = {
+  controllers: PropTypes.object
+};
+
+describe('ProviderController', () => {
+  it('should put the given controller in the context', () => {
+    const component = mount(
+      <ProvideController controller={{name: 'testController'}}>
+        <ChildComponent />
+      </ProvideController>);
+      expect(component.find('[data-hook="controllerName"]').text()).toEqual('testController');
+  });
+});
