61debf94720e3c77e1c1e30303fd5b0f79223296
Nir Yosef <niryo@wix.com>
2017-11-25 13:03:56 +0200

    unify proxy test and non proxy tests

--- src/Controller/Controller.spec.js
+++ src/Controller/Controller.spec.js
@@ -50,10 +50,6 @@ class ParentController extends Controller {
     return this.state.dynamicObject;
   }
 
-  changeNameOfObjectProp() {
-    this.state.objectProp.name = 'changed!';
-  }
-
   addArrayToDynamicObject() {
     this.state.dynamicObject.array = [];
   }
@@ -61,6 +57,7 @@ class ParentController extends Controller {
     this.state.dynamicObject.array.push('alice');
   }
   changeMultiPropsButton() {
+    this.state.basicProp = Math.random();
     this.state.basicProp = Math.random();
     this.state.objectProp.name = Math.random();
     this.state.dynamicObject.foo = Math.random();
@@ -88,7 +85,6 @@ class Parent extends React.Component {
         <div data-hook="basicPropPreview">{this.controller.getBasicProp()}</div>
         <button data-hook="changeBasicPropButton" onClick={() => this.controller.changeBasicProp()} />
         <div data-hook="objectPropPreviw">{JSON.stringify(this.controller.getObjectProp())}</div>
-        <button data-hook="changeNameButton" onClick={() => this.controller.changeNameOfObjectProp()} />
         <div data-hook="dynamicObjectPreviw">{JSON.stringify(this.controller.getDynamicObject())}</div>
         <button data-hook="addArrayToDynamicObjectButton" onClick={() => this.controller.addArrayToDynamicObject()} />
         <button data-hook="addNameToDynamicObjectArrayButton" onClick={() => this.controller.addNameToDynamicObjectArray()} />
@@ -164,9 +160,9 @@ describe('Controller', () => {
     const controller = new TestStateInitController(Parent);
     controller.setState({ hello: true });
     expect(controller.getState()).toEqual({ hello: true });
-    expect(() =>  controller.setState(true) ).toThrowError('State should be initialize only with plain object');
+    expect(() => controller.setState(true)).toThrowError('State should be initialize only with plain object');
     expect(() => controller.setState(['1', '2'])).toThrowError('State should be initialize only with plain object');
-    expect(() => controller.setState(() => {})).toThrowError('State should be initialize only with plain object');
+    expect(() => controller.setState(() => { })).toThrowError('State should be initialize only with plain object');
     expect(() => controller.setState('string')).toThrowError('State should be initialize only with plain object');
     expect(() => controller.setState(['1', '2'])).toThrowError('State should be initialize only with plain object');
   });
@@ -179,91 +175,73 @@ describe('Controller', () => {
     expect(() => testController.state.bla = 'bla').toThrowError('Cannot touch state outside of controller');
   });
 
-  describe('Complex tests', () => {
-    it('e2e test', () => {
-      const component = mount(<Parent />);
-      expect(component.find('[data-hook="blamos"]').text()).toEqual('blamos');
+  describe('Tests with mounting component', () => {
+    const backupProxy =  global.Proxy;
+  
+    describe('without Proxy', () => {
+      beforeEach(() => {
+        global.Proxy = undefined;
+        parentComponentRenderCount = 0;      
+      });
+  
+      afterEach(() => {
+        global.Proxy = backupProxy;
+      });
+    
+      runTests();
+  
     });
-
-    it('should have an observable state', () => {
-      const OberverParent = observer(Parent);
-      const component = mount(<OberverParent />);
-      expect(component.find('[data-hook="basicPropPreview"]').text()).toEqual('blamos');
-      component.find('[data-hook="changeBasicPropButton"]').simulate('click');
-      expect(component.find('[data-hook="basicPropPreview"]').text()).toEqual('changed!');
+    describe('with Proxy', () => {
+      beforeEach(() => {
+        parentComponentRenderCount = 0;
+      });
+      runTests();
     });
-
-    it('should observe on deep nested change', () => {
-      const OberverParent = observer(Parent);
-      const component = mount(<OberverParent />);
-      expect(component.find('[data-hook="dynamicObjectPreviw"]').text()).toEqual('{}');
-      component.find('[data-hook="addArrayToDynamicObjectButton"]').simulate('click');
-      expect(component.find('[data-hook="dynamicObjectPreviw"]').text()).toEqual(JSON.stringify({ array: [] }));
-      component.find('[data-hook="addNameToDynamicObjectArrayButton"]').simulate('click');
-      expect(component.find('[data-hook="dynamicObjectPreviw"]').text()).toEqual(JSON.stringify({ array: ['alice'] }));
-    });
-
-    //todo: try to fail the test
-    it('should trigger only one render per setter', () => {
-      const OberverParent = observer(Parent);
-      const component = mount(<OberverParent />);
-      expect(parentComponentRenderCount).toEqual(1);
-      component.find('[data-hook="changeMultiPropsButton"]').simulate('click');
-      expect(parentComponentRenderCount).toEqual(2);
-    });
-
-    it('should allow setters with args', () => {
-      const OberverParent = observer(Parent);
-      const component = mount(<OberverParent />);
-      component.find('[data-hook="applySetterWithArgsButton"]').simulate('click');
-      expect(component.find('[data-hook="basicPropPreview"]').text()).toEqual('value1value2');
-    });
-  });
-
-
-  describe('when proxy is not availble', () => {
-    let backupProxy;
-    beforeEach(() => {
-      backupProxy = global.Proxy;
-      global.Proxy = undefined;
-    });
-
-    it('should have an observable state', () => {
-      const OberverParent = observer(Parent);
-      const component = mount(<OberverParent />);
-      expect(component.find('[data-hook="basicPropPreview"]').text()).toEqual('blamos');
-      global.Proxy = backupProxy;
-      component.find('[data-hook="changeBasicPropButton"]').simulate('click');
-      expect(component.find('[data-hook="basicPropPreview"]').text()).toEqual('changed!');
-    });
-
-    it('should observe on deep nested change', () => {
-      const OberverParent = observer(Parent);
-      const component = mount(<OberverParent />);
-      expect(component.find('[data-hook="dynamicObjectPreviw"]').text()).toEqual('{}');
-      global.Proxy = backupProxy;
-      component.find('[data-hook="addArrayToDynamicObjectButton"]').simulate('click');
-      expect(component.find('[data-hook="dynamicObjectPreviw"]').text()).toEqual(JSON.stringify({ array: [] }));
-      component.find('[data-hook="addNameToDynamicObjectArrayButton"]').simulate('click');
-      expect(component.find('[data-hook="dynamicObjectPreviw"]').text()).toEqual(JSON.stringify({ array: ['alice'] }));
-    });
-
-    //todo: try to fail the test
-    it('should trigger only one render per setter', () => {
-      const OberverParent = observer(Parent);
-      const component = mount(<OberverParent />);
-      expect(parentComponentRenderCount).toEqual(2); //todo: why 2? why we are wasting a render
-      global.Proxy = backupProxy;
-      component.find('[data-hook="changeMultiPropsButton"]').simulate('click');
-      expect(parentComponentRenderCount).toEqual(3);
-    });
-
-    it('should allow setters with args', () => {
-      const OberverParent = observer(Parent);
-      const component = mount(<OberverParent />);
-      global.Proxy = backupProxy;
-      component.find('[data-hook="applySetterWithArgsButton"]').simulate('click');
-      expect(component.find('[data-hook="basicPropPreview"]').text()).toEqual('value1value2');
-    });
+  
+    function runTests() {
+      it('sanity check', () => {
+        const component = mount(<Parent />);
+        expect(component.find('[data-hook="blamos"]').text()).toEqual('blamos');
+      });
+  
+      it('should have an observable state', () => {
+        const OberverParent = observer(Parent);
+        const component = mount(<OberverParent />);
+        expect(component.find('[data-hook="basicPropPreview"]').text()).toEqual('blamos');
+        global.Proxy = backupProxy;
+        component.find('[data-hook="changeBasicPropButton"]').simulate('click');
+        expect(component.find('[data-hook="basicPropPreview"]').text()).toEqual('changed!');
+      });
+  
+      it('should observe on deep nested change', () => {
+        const OberverParent = observer(Parent);
+        const component = mount(<OberverParent />);
+        expect(component.find('[data-hook="dynamicObjectPreviw"]').text()).toEqual('{}');
+        global.Proxy = backupProxy;
+        component.find('[data-hook="addArrayToDynamicObjectButton"]').simulate('click');
+        expect(component.find('[data-hook="dynamicObjectPreviw"]').text()).toEqual(JSON.stringify({ array: [] }));
+        component.find('[data-hook="addNameToDynamicObjectArrayButton"]').simulate('click');
+        expect(component.find('[data-hook="dynamicObjectPreviw"]').text()).toEqual(JSON.stringify({ array: ['alice'] }));
+      });
+  
+      // todo: try to fail the test
+      it('should trigger only one render per setter', () => {
+        const OberverParent = observer(Parent);
+        const component = mount(<OberverParent />);
+        expect(parentComponentRenderCount).toBeLessThanOrEqual(2);
+        global.Proxy = backupProxy;
+        component.find('[data-hook="changeMultiPropsButton"]').simulate('click');
+        expect(parentComponentRenderCount).toBeLessThanOrEqual(3);
+      });
+  
+      it('should allow setters with args', () => {
+        const ObserverParent = observer(Parent);
+        const component = mount(<ObserverParent />);
+        global.Proxy = backupProxy;
+        component.find('[data-hook="applySetterWithArgsButton"]').simulate('click');
+        expect(component.find('[data-hook="basicPropPreview"]').text()).toEqual('value1value2');
+      });
+    }
   });
 });
+
