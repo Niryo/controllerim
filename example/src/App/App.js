acf94ab56fb68a104c7de182beea47c8583fe7c9
Nir Yosef <niryo@wix.com>
2017-11-28 17:03:24 +0200

    update example

--- example/src/App/App.js
+++ example/src/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import {NotesList} from '../NotesList/NotesList';
-import { observer, ProvideController } from 'controllerim';
+import { NotesList } from '../NotesList/NotesList';
+import { observer } from 'controllerim';
 
 import { AppController } from './AppController';
 
@@ -12,25 +12,23 @@ export const App = observer(class extends Component {
 
   render() {
     return (
-      <ProvideController controller={this.controller}>
-        <div className="appContainer">
-          <h1>This is an example of multiple instacne of the same component (with different themes)</h1>
-          <h2 data-hook="counter">Total notes count: {this.controller.getTotalNotesCount()}</h2>
-          <label className="userNameInputLabel">Enter Your Name:</label>
-          <input
-            className="userNameInput"
-            value={this.controller.getUserName()}
-            onChange={(e) => this.controller.setUserName(e.target.value)} />
-          <div className="notesContainer">
-            <div className="leftNote">
-              <NotesList theme={'theme1'} />
-            </div>
-            <div>
-              <NotesList theme={'theme2'} />
-            </div>
+      <div className="appContainer">
+        <h1>This is an example of multiple instacne of the same component (with different themes)</h1>
+        <h2 data-hook="counter">Total notes count: {this.controller.getTotalNotesCount()}</h2>
+        <label className="userNameInputLabel">Enter Your Name:</label>
+        <input
+          className="userNameInput"
+          value={this.controller.getUserName()}
+          onChange={(e) => this.controller.setUserName(e.target.value)} />
+        <div className="notesContainer">
+          <div className="leftNote">
+            <NotesList theme={'theme1'} />
+          </div>
+          <div>
+            <NotesList theme={'theme2'} />
           </div>
         </div>
-      </ProvideController>
+      </div>
     );
   }
 });
