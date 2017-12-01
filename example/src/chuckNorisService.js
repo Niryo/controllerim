4f8269d9c0e8949d83ddb23f83cc1a4e37121fc6
Nir Yosef <niryo@wix.com>
2017-12-01 19:18:52 +0200

    fix example

--- example/src/chuckNorisService.js
+++ example/src/chuckNorisService.js
@@ -1,5 +1,5 @@
 export const fetchRandomJoke = async () => {
-  const response = await fetch('http://api.icndb.com/jokes/random');
+  const response = await fetch('https://api.icndb.com/jokes/random');
   const result = await response.json();
   return result.value.joke;
 };
