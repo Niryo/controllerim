33c0f90a26b21b2d44fe08845a917949c62d7584
Nir Yosef <niryo@wix.com>
2020-05-02 00:24:31 +0300

    Updates

---
+++ precache-manifest.119b13fff375897adf0ca9cdaef41007.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da40bf15feab65517ea3f0bbb04e704",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfdc03f5581d7efa9837",
+    "url": "/static/css/main.26bb336d.chunk.css"
+  },
+  {
+    "revision": "197116cb28ae187cd906",
+    "url": "/static/js/2.ebfe0f63.chunk.js"
+  },
+  {
+    "revision": "806a98f318263a7af6eb94d73465b791",
+    "url": "/static/js/2.ebfe0f63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dfdc03f5581d7efa9837",
+    "url": "/static/js/main.ded69f50.chunk.js"
+  },
+  {
+    "revision": "0c0586a3e81c75c45096",
+    "url": "/static/js/runtime-main.47adf6c7.js"
+  }
+]);
