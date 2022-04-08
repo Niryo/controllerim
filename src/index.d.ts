1482a2e62a26f04469974b670084c40b96f93ebe
Nir Yosef <niryo@wix.com>
2022-04-08 15:49:38 +0300

    fix type

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,7 +3,7 @@ interface InstanceFactory<C> {
   getInstance: (id?: string) => C;
 }
 export type ControllerInstanceType<C extends InstanceFactory<any>> = ReturnType<C["create"]>;
-export function controller<C extends {state: object}>(c: {new(): C}): InstanceFactory<C>;
+export function controller<C>(c: {new(): C}): InstanceFactory<C>;
 export function useController<C>(c: C): C;
 export function store<C>(c: {new(): C}): C;
 export function observer<Comp>(component: Comp): Comp;
